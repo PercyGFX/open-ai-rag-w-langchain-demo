a67759d4d1e85b7bd8eb24ebc86b11be2646aa9e
PercyGFX <isurangabtk@gmail.com>
2025-01-14 01:25:18 +0530

    prompt templating and chaining

--- src/controllers/langchain.controller.ts
+++ src/controllers/langchain.controller.ts
@@ -1,8 +1,10 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { Request, Response } from "express";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
 import Joi from "joi";
 
-const openai = new ChatOpenAI({
+const model = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   model: "gpt-3.5-turbo",
   temperature: 0.5,
@@ -15,11 +17,23 @@ const langchainSchema = Joi.object({
 export const langChainchat = async (req: Request, res: Response) => {
   const { error, value } = langchainSchema.validate(req.body);
 
+  const prompt = ChatPromptTemplate.fromTemplate(
+    "tell a joke bout from {message}"
+  );
+
+  const chain = prompt.pipe(model);
+
+  const response = await chain.invoke({
+    message: value.message,
+  });
+
+  console.log(response);
+
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
 
-  const response = await openai.invoke(value.message);
+  //const response = await openai.invoke(value.message);
 
-  console.log(response.content);
+  //console.log(response.content);
 };
